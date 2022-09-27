ba5db8bc5e28945f3a18053f063865d299bc4969
doaamahmoud <doaamahmoud048@gmail.com>
2022-09-27 13:07:47 +0200

    update project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Components/about/about.component';
+import { ArticleComponent } from './Components/article/article.component';
 import { LayoutComponent } from './Components/layout/layout.component';
+import { ProductsComponent } from './Components/products/products.component';
 
 const routes: Routes = [
   
   {path:'', redirectTo:'Layout',pathMatch:'full'},
   {path:'Layout', component:LayoutComponent},
+  {path:'Products', component:ProductsComponent},
+  {path:'Article', component:ArticleComponent},
+  {path:'About',component:AboutComponent},
  
   {path:'**',redirectTo:'/Layout'},
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { LayoutComponent } from './Components/layout/layout.component';
 import { HomeComponent } from './Components/home/home.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ProductsComponent } from './Components/products/products.component';
+import { ArticleComponent } from './Components/article/article.component';
+import { AboutComponent } from './Components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LayoutComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    ProductsComponent,
+    ArticleComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
